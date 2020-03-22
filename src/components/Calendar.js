10355fb459e2f1af7b49955985ba701615d57ef8
Sean Scott <sean@icansee.co.uk>
2020-03-22 23:20:19 +0000

    changes

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -115,7 +115,7 @@ class Calendar extends React.Component {
   renderSelected() {
     return (
       <div className="calendar__selected">
-        <span className="calendar__selected__date">Timeslot One: ß{this.state.dateOne}</span>
+        <span className="calendar__selected__date">Timeslot One: {this.state.dateOne}</span>
         <span className="calendar__selected__date">Timeslot Two: {this.state.dateTwo}</span>
         <span className="calendar__selected__date">Timeslot Three: {this.state.dateThree}</span>
       </div>
